fbaba971a8d2bfc6b59d994ffaec476e715c9f38
Fahim Wayez <fahimwayez1@gmail.com>
2023-11-01 23:12:00 +0600

    video background problem solved

--- JS/landingPageScript.js
+++ JS/landingPageScript.js
@@ -1,20 +1,36 @@
 const buttons = document.querySelectorAll(".navButton");
-const slides = document.querySelectorAll("videoSlide");
+const slides = document.querySelectorAll(".videoSlide");
+
 const contents = document.querySelectorAll(".content");
+const logoVideo = document.getElementById("logoVideo");
 
 var sliderNav = function (manual) {
-    buttons.forEach((btn) => { btn.classList.remove("active"); });
+    buttons.forEach((btn) => {
+        btn.classList.remove("active");
+    });
 
     slides.forEach((slide) => {
         slide.classList.remove("active");
     });
 
     contents.forEach((content) => {
-        slide.classList.remove("active");
+        content.classList.remove("active");
     });
+
+    buttons[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
 }
 buttons.forEach((btn, i) => {
     btn.addEventListener("click", () => {
         sliderNav(i);
     });
+});
+
+logoVideo.addEventListener("mouseenter", () => {
+    logoVideo.play();
+});
+
+logoVideo.addEventListener("mouseleave", () => {
+    logoVideo.stop();
 });
