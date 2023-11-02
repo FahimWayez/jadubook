d6899bf2c8d0a8857d75fb912ad72f6a425f8d22
Fahim Wayez <fahimwayez1@gmail.com>
2023-11-02 06:41:49 +0600

    commenting

--- JS/loadingAnimation.js
+++ JS/loadingAnimation.js
@@ -4,3 +4,12 @@ loader.style.display = "block";
 setTimeout(function () {
     loader.style.display = "none";
 }, 2000);
+
+// in html:
+
+// just after body: <div id="loadingAnimation"></div>
+// just before ending body: <script src="../JS/loadingAnimation.js"></script>
+
+
+// in css:
+// @import url('loadingAnimation.css');
