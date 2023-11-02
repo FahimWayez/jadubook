834c88ee39942ce90bcfa3f2ee061fb239b4017e
Fahim Wayez <fahimwayez1@gmail.com>
2023-11-02 06:46:06 +0600

    loading added while going back

--- JS/goBack.js
+++ JS/goBack.js
@@ -1,3 +1,9 @@
+var loader = document.getElementById("loadingAnimation");
 function goBack() {
-    window.history.back();
+    loader.style.display = "block";
+
+    setTimeout(function () {
+        loader.style.display = "none";
+        window.history.back();
+    }, 1000);
 }
