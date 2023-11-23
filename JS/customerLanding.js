192383fbff1ab8d06998ae72c7b11df34912be76
Fahim Wayez <fahimwayez1@gmail.com>
2023-11-23 13:02:36 +0600

    customer landing design updated

--- JS/customerLanding.js
+++ JS/customerLanding.js
@@ -1,18 +1,19 @@
+const logoVideo = document.getElementById("logoVideo");
+
 searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
-let loginForm = document.querySelector('.login-form-container');
+logoVideo.addEventListener("mouseenter", () => {
+    logoVideo.play();
+});
 
-document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
-}
+logoVideo.addEventListener("mouseleave", () => {
+    logoVideo.stop();
+});
 
-document.querySelector('#close-login-btn').onclick = () => {
-    loginForm.classList.remove('active');
-}
 
 window.onscroll = () => {
 
@@ -23,7 +24,6 @@ window.onscroll = () => {
     } else {
         document.querySelector('.header .header-2').classList.remove('active');
     }
-
 }
 
 window.onload = () => {
@@ -35,82 +35,4 @@ window.onload = () => {
     }
 
     fadeOut();
-
 }
-
-function loader() {
-    document.querySelector('.loader-container').classList.add('active');
-}
-
-function fadeOut() {
-    setTimeout(loader, 4000);
-}
-
-var swiper = new Swiper(".books-slider", {
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".featured-slider", {
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        450: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        1024: {
-            slidesPerView: 4,
-        },
-    },
-});
-var swiper = new Swiper(".reviews-slider", {
-    spaceBetween: 10,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
