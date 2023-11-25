2020509939c6d7d1d738d8fe26ef38371d508edd
Fahim Wayez <fahimwayez1@gmail.com>
2023-11-25 15:54:57 +0600

    book page changing effect added

--- JS/book1.js
+++ JS/book1.js
@@ -6,29 +6,6 @@ const logoVideo = document.getElementById("logoVideo");
 const exploreLink = document.getElementById("exploreLink");
 const contents = document.querySelectorAll(".content");
 
-var sliderNav = function (manual) {
-    buttons.forEach((btn) => {
-        btn.classList.remove("active");
-    });
-
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-
-    contents.forEach((content) => {
-        content.classList.remove("active");
-    });
-
-    buttons[manual].classList.add("active");
-    slides[manual].classList.add("active");
-    contents[manual].classList.add("active");
-}
-buttons.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        sliderNav(i);
-    });
-});
-
 logoVideo.addEventListener("mouseenter", () => {
     logoVideo.play();
 });
@@ -55,3 +32,10 @@ var sliderNav = function (manual) {
     contents[manual].classList.add("active");
     contents[manual].parentElement.querySelector('.text-container').classList.add("active");
 }
+
+buttons.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+
